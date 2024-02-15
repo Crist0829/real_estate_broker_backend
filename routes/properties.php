a63375cffa80538132b9b03474065f93fa46a3bf
Crist <ccjd.0796@gmail.com>
2024-02-15 04:19:33 -0300

    Corrijo método de la ruta store de las properties

--- routes/properties.php
+++ routes/properties.php
@@ -7,6 +7,6 @@ Route::middleware(['auth:sanctum'])->group(function (){
 
     Route::get('/properties', [PropertyController::class, 'index']);
     Route::get('/all-properties', [PropertyController::class, 'indexAll']);
-    Route::get('/properties/store', [PropertyController::class, 'store']);
+    Route::post('/properties/store', [PropertyController::class, 'store']);
 });
 
